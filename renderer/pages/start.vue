fa063f48f8a053c7f9e837ecaadebcf1376d243b
HamidNE <hamidne@mail.ru>
2019-12-21 16:36:05 +0330

    add wizard for project

--- renderer/pages/start.vue
+++ renderer/pages/start.vue
@@ -1,26 +1,57 @@
 <template>
-  <div class="container">
-    <img src="/nuxt-black.png">
-    <h2>Thank you for testing Nuxtron</h2>
-    <p>
-      <NuxtLink to="/">
-        Back home
-      </NuxtLink>
-    </p>
-  </div>
+	<div
+		class="container d-flex align-items-center justify-content-center flex-column min-vh-100"
+	>
+		<form-wizard
+			stepSize="sm"
+			class="w-100"
+			title=""
+      shape="tab"
+			subtitle=""
+			nextButtonText="بعدی"
+			backButtonText="قبلی"
+			finishButtonText="پایان"
+			@on-complete="complete"
+		>
+			<tab-content title="انتخاب نوع">
+				انتخاب نوع
+			</tab-content>
+			<tab-content title="انتخاب عنوان">
+				انتخاب عنوان
+			</tab-content>
+			<tab-content title="انتخاب توضیحات">
+				انتخاب توضیحات
+			</tab-content>
+		</form-wizard>
+	</div>
 </template>
 
 <script>
+import { FormWizard, TabContent } from "vue-form-wizard";
 export default {
-}
+	components: {
+		FormWizard,
+		TabContent
+	},
+	data: () => ({
+		form: {}
+	}),
+	methods: {
+		complete() {
+			console.log("asdasdasdasd");
+		}
+	}
+};
 </script>
 
-<style scoped>
-.container {
-  display: flex;
-	min-height: 100vh;
-	align-items: center;
-	flex-direction: column;
-	justify-content: center;
+<style src="vue-form-wizard/dist/vue-form-wizard.min.css"></style>
+<style lang="scss">
+.vue-form-wizard {
+	.wizard-nav {
+		padding: 0;
+	}
+	.wizard-progress-bar {
+		float: right;
+	}
 }
 </style>
