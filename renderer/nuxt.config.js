de6320c7eba06b6464001ea28246627bc3b5d3f2
HamidNE <hamidne@mail.ru>
2019-12-21 12:55:36 +0330

    add module config file

--- renderer/nuxt.config.js
+++ renderer/nuxt.config.js
@@ -1,9 +1,12 @@
 import css from './config/css';
 import head from './config/head';
+import modules from './config/modules';
 
 export default {
   css,
   head,
+  modules,
+  
   build: {
     extend: (config) => {
       config.target = 'electron-renderer';
